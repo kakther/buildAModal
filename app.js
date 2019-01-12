579cc99f4836f60e01eb23700207472b9103c542
Kausar Akther <aktherkaur@gmail.com>
2019-01-12 12:17:46 -0500

    initiallze git

---
+++ app.js
@@ -0,0 +1,21 @@
+const openModelTriggerEl = document.querySelector(".trigger");
+const closeModalTriggerEl = document.querySelector(".close");
+const modalEl = document.querySelector(".modal");
+
+function main() {
+    openModelTriggerEl.addEventListener("click", function() {
+        modalEl.classList.add("open");
+ }) 
+
+    closeModalTriggerEl.addEventListener("click", function() {
+        modalEl.classList.remove("open");
+})
+
+window.addEventListener("click", function(event) {
+    if (event.target === modalEl) {
+        modalEl.classList.remove("open");
+     }
+    })
+}
+
+main();
